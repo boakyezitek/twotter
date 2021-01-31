847e82cb2444b8526af29956d5f0f3ad0fb09aa6
Kofi Boakye <66253143+boakyezitek@users.noreply.github.com>
2021-01-31 17:57:40 +0000

    vue template setup

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+<div id="App">
+  <UserProfile />
+</div>
+  
+</template>
+
+<script>
+import UserProfile from './components/UserProfile.vue'
+
+export default {
+  name: 'App',
+  components:{UserProfile},
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/UserProfile.vue
@@ -0,0 +1,68 @@
+<template>
+<div class="user-profile">
+   @ {{user.username}} - {{fullname}}
+   <br/> <span>Followers: </span> <strong>{{fellowers}}</strong>
+  <br/> <button @click="followUser">Fellow</button>
+
+</div>
+
+</template>
+
+<script>
+export default {
+  name: 'UserProfile',
+   data(){
+    return {
+      fellowers:0,
+      user:{
+        id:1, 
+        username: 'boakye51',
+        firstname: 'boakye',
+        lastname: 'kofi'
+
+      }
+    }
+  },
+
+  computed: {
+    fullname(){
+      return  `${this.user.firstname} ${this.user.lastname}`
+    }
+  },
+
+  methods: {
+    followUser() {
+      this.fellowers ++  
+    }
+  } ,
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ .user-profle{
+   display: grid;
+   grid-template-columns: 1fr 3fr;
+   width: 50% 5%;
+   padding: 50px 5%;
+
+  
+ }
+
+  .user-profile__user-panel{
+    display: flex;
+    flex-direction: column;
+    margin-right: 50px;
+    padding: 20px;
+    background-color: white;
+    border-radius: 5px;
+    border:1px solid #DFE3F8;
+  }
+
+  h1{
+    margin:0px;
+  }
+</style>
